32e4beb7bf3fd5fd62503b4f09cd0a93df19575a
Crystal Soto <crystal@texasautovalue.com>
2023-07-18 15:51:12 -0500

    added style to index and removed duplicated index

--- PureInvestWebsite/wwwroot/js/site.js
+++ PureInvestWebsite/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var buyNowButton = document.getElementById("buyNowButton");
+    if (buyNowButton) {
+        buyNowButton.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent the form from submitting
 
-// Write your JavaScript code.
+            // Perform the desired action here
+            // Example: Display a message or make an API request
+
+            // Replace the following line with your desired logic
+            alert("Purchase successful!");
+        });
+    }
+});
